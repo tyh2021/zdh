af328914d73f41c43526f63bf5a96dcffdfe42dc
569213710@qq.com <tyh2019>
2021-03-16 14:32:04 +0800

    提交代码14:31

---
+++ yfw/conftest.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+import pytest
+import requests
+
+
+h={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+    "Authorization":"bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6bnVsbCwicmVzb3VyY2VzIjpbXSwiY2xpZW50X2lkIjoidGVjc3VuX3BjX2NsaWVudCIsImF1ZCI6WyJvcmRlciJdLCJwYXNzd29yZCI6IiQyYSQxMCRabkJDbkY3TmxYOUtNb0VsclNVWUouLm9SRGRxOVhSVTlacmVCR1ovcDZPQlBsNy55ZGFDZSIsInNjb3BlIjpbInJlYWQiXSwib3JnYW5pemF0aW9uIjoiMzk2MzEzNzcwNTIzNDc1OTY4IiwibmFtZSI6IuiwreaciOWNjiIsImlkIjo0NTAwODQ5NTIxNjM1Njk2NjQsInVzZXJUeXBlIjozLCJleHAiOjE2MTQ5Mzc4MTQsImVtYWlsIjoiIiwianRpIjoiNWVjZjAzYjYxMDRhNDAyYTg5ZTE2NmE0YmM3OTU5ZjMiLCJ1c2VybmFtZSI6InRhbnl1ZWh1YSJ9.iBwYPZzWkKz3NBIIUO4E6SfT7x0Yf3UgDir7FhmC0Wo"
+    }
+# @pytest.fixture()
+# def login():
+#     print("输入账号，密码先登录")
+
+print("hellp")
